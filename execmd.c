b2037c26496f79b4b500beddc0307d1c04ec47d6
ahmed_abdelrahman <ahmedebrahimz930@gmail.com>
2023-07-14 11:13:47 -0700

    execmd.c

--- execmd.c
+++ execmd.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include "get_location.h"
 
 void execmd(char **argv) {
     char *command = NULL;
@@ -11,27 +12,23 @@ void execmd(char **argv) {
         /* get the command */
         command = argv[0];
 
-        /* handle specific commands */
         if (strcmp(command, "ls") == 0) {
-            /* check if "-la" option is provided */
-            if (argv[1] && strcmp(argv[1], "-la") == 0) {
-                /* execute ls -la command */
-                argv[0] = "/bin/ls"; // or provide the path to ls executable
-                if (execvp(argv[0], argv) == -1) {
-                    perror("Error:");
-                }
-            } else {
-                /* execute ls command */
-                if (execvp(argv[0], argv) == -1) {
-                    perror("Error:");
-                }
-            }
-        } else {
-            /* execute other commands with execvp */
-            if (execvp(command, argv) == -1) {
-                perror("Error:");
-            }
+    /* check if "-la" option is provided */
+    if (argv[1] && strcmp(argv[1], "-la") == 0) {
+        /* execute ls -la command */
+        argv[0] = "/bin/ls"; // or provide the path to ls executable
+        argv[1] = "-la"; // set the second argument to -la
+        if (execvp(argv[0], argv) == -1) {
+            perror("Error:");
         }
+    } else {
+        /* execute ls command */
+        if (execvp(argv[0], argv) == -1) {
+            perror("Error:");
+        }
+    }
+}
+
     }
 }
 
