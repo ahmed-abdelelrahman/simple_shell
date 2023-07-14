889ca91d180ac8811d9fa8a67d19b561b2895946
minarob23 <menarobir246rm@gmail.com>
2023-07-14 16:12:43 -0700

    mina

--- get_location.h
+++
@@ -1,7 +0,0 @@
-#ifndef GET_LOCATION_H
-#define GET_LOCATION_H
-
-char *get_location(char *command);
-
-#endif
-
--- main.c
+++ main.c
@@ -1,68 +1,44 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <string.h>
-#include <stdlib.h>
-#include "execmd.h"
+#include "shell.h"
 
-#define MAX_INPUT_LENGTH 100
+/**
+ * main - entry point
+ * @ac: arg count
+ * @av: arg vector
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int ac, char **av)
+{
+	info_t info[] = { INFO_INIT };
+	int fd = 2;
 
-int main(void) {
-  char *prompt = "(Eshell) $ ";
-  char *full_command = NULL;
-  size_t n = 0;
-  ssize_t nchars_read;
-  char *token;
-  const char *delim = " \n";
-  char **argv;
-  int i = 0;
-  int num_tokens = 0;
+	asm ("mov %1, %0\n\t"
+		"add $3, %0"
+		: "=r" (fd)
+		: "r" (fd));
 
-  while (1) {
-    printf("%s", prompt);
-    fflush(stdout);
-
-    nchars_read = getline(&full_command, &n, stdin);
-
-    if (nchars_read == -1) {
-      printf("Exiting shell....\n");
-      break;
-    } else {
-      token = strtok(full_command, delim);
-
-      while (token != NULL) {
-        num_tokens++;
-        token = strtok(NULL, delim);
-      }
-      num_tokens++;
-
-      argv = malloc(sizeof(char *) * num_tokens);
-
-      token = strtok(full_command, delim);
-
-      for (i = 0; token != NULL; i++) {
-        argv[i] = malloc(sizeof(char) * (strlen(token) + 1));
-        strcpy(argv[i], token);
-        token = strtok(NULL, delim);
-      }
-
-      /* Remove newline character from the last token if present*/
-      if (i > 0 && argv[i - 1][strlen(argv[i - 1]) - 1] == '\n') {
-        argv[i - 1][strlen(argv[i - 1]) - 1] = '\0';
-      }
-
-      argv[i] = NULL;
-
-      execmd(argv);
-
-      for (i = 0; argv[i] != NULL; i++) {
-        free(argv[i]);
-      }
-      free(argv);
-    }
-  }
-
-  free(full_command);
-
-  return 0;
+	if (ac == 2)
+	{
+		fd = open(av[1], O_RDONLY);
+		if (fd == -1)
+		{
+			if (errno == EACCES)
+				exit(126);
+			if (errno == ENOENT)
+			{
+				_eputs(av[0]);
+				_eputs(": 0: Can't open ");
+				_eputs(av[1]);
+				_eputchar('\n');
+				_eputchar(BUF_FLUSH);
+				exit(127);
+			}
+			return (EXIT_FAILURE);
+		}
+		info->readfd = fd;
+	}
+	populate_env_list(info);
+	read_history(info);
+	hsh(info, av);
+	return (EXIT_SUCCESS);
 }
-
