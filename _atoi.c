b843c49defaf89b5926a4e6257f5472cd8527b28
Mina Robir <menarobir246rm@gmail.com>
2023-07-15 03:08:02 +0300

    mina

--- _atoi.c
+++ _atoi.c
@@ -1,37 +1,38 @@
 #include "shell.h"
 
 /**
- * interactive - returns true if shell is interactive mode
+ * is_interactive - returns true if shell is in interactive mode
  * @info: struct address
  *
  * Return: 1 if interactive mode, 0 otherwise
  */
-int interactive(info_t *info)
+int is_interactive(info_t *info)
 {
 	return (isatty(STDIN_FILENO) && info->readfd <= 2);
 }
 
 /**
- * is_delim - checks if character is a delimeter
+ * is_delimiter - checks if character is a delimiter
  * @c: the char to check
- * @delim: the delimeter string
+ * @delim: the delimiter string
  * Return: 1 if true, 0 if false
  */
-int is_delim(char c, char *delim)
+int is_delimiter(char c, char *delim)
 {
 	while (*delim)
+	{
 		if (*delim++ == c)
 			return (1);
+	}
 	return (0);
 }
 
 /**
- *_isalpha - checks for alphabetic character
- *@c: The character to input
- *Return: 1 if c is alphabetic, 0 otherwise
+ * is_alpha - checks for alphabetic character
+ * @c: The character to input
+ * Return: 1 if c is alphabetic, 0 otherwise
  */
-
-int _isalpha(int c)
+int is_alpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
@@ -40,17 +41,16 @@ int _isalpha(int c)
 }
 
 /**
- *_atoi - converts a string to an integer
- *@s: the string to be converted
- *Return: 0 if no numbers in string, converted number otherwise
+ * my_atoi - converts a string to an integer
+ * @s: the string to be converted
+ * Return: 0 if no numbers in string, converted number otherwise
  */
-
-int _atoi(char *s)
+int my_atoi(char *s)
 {
 	int i, sign = 1, flag = 0, output;
 	unsigned int result = 0;
 
-	for (i = 0;  s[i] != '\0' && flag != 2; i++)
+	for (i = 0; s[i] != '\0' && flag != 2; i++)
 	{
 		if (s[i] == '-')
 			sign *= -1;
