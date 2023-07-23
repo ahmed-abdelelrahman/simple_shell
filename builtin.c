0a25ed0f1e8f90b90d4ec70b89df0ce5644bdd50
minarob23 <menarobir246rm@gmail.com>
2023-07-23 09:24:49 -0700

    mina

--- builtin.c
+++ builtin.c
@@ -1,25 +1,25 @@
 #include "shell.h"
 
 /**
- * _myexit - exits the shell
+ * custom_myexit - exits the shell
  * @info: Structure containing potential arguments. Used to maintain
  *          constant function prototype.
  *  Return: exits with a given exit status
  *         (0) if info.argv[0] != "exit"
  */
-int _myexit(info_t *info)
+int custom_myexit(info_t *info)
 {
 	int exitcheck;
 
 	if (info->argv[1])  /* If there is an exit arguement */
 	{
-		exitcheck = error_atoi(info->argv[1]);
+		exitcheck = _erratoi(info->argv[1]);
 		if (exitcheck == -1)
 		{
 			info->status = 2;
 			print_error(info, "Illegal number: ");
-			puts(info->argv[1]);
-			putchar('\n');
+			_eputs(info->argv[1]);
+			_eputchar('\n');
 			return (1);
 		}
 		info->err_num = _erratoi(info->argv[1]);
@@ -30,22 +30,22 @@ int _myexit(info_t *info)
 }
 
 /**
- * _mycd - changes the current directory of the process
+ * custom_mycd - changes the current directory of the process
  * @info: Structure containing potential arguments. Used to maintain
  *          constant function prototype.
  *  Return: Always 0
  */
-int _mycd(info_t *info)
+int custom_mycd(info_t *info)
 {
 	char *s, *dir, buffer[1024];
 	int chdir_ret;
 
 	s = getcwd(buffer, 1024);
 	if (!s)
-		puts("TODO: >>getcwd failure emsg here<<\n");
+		_puts("TODO: >>getcwd failure emsg here<<\n");
 	if (!info->argv[1])
 	{
-		dir = getenv(info, "HOME=");
+		dir = _getenv(info, "HOME=");
 		if (!dir)
 			chdir_ret = /* TODO: what should this be? */
 				chdir((dir = _getenv(info, "PWD=")) ? dir : "/");
@@ -57,7 +57,7 @@ int _mycd(info_t *info)
 		if (!_getenv(info, "OLDPWD="))
 		{
 			_puts(s);
-			putchar('\n');
+			_putchar('\n');
 			return (1);
 		}
 		_puts(_getenv(info, "OLDPWD=")), _putchar('\n');
@@ -73,19 +73,19 @@ int _mycd(info_t *info)
 	}
 	else
 	{
-		setenv(info, "OLDPWD", _getenv(info, "PWD="));
+		_setenv(info, "OLDPWD", _getenv(info, "PWD="));
 		_setenv(info, "PWD", getcwd(buffer, 1024));
 	}
 	return (0);
 }
 
 /**
- * _myhelp - changes the current directory of the process
+ * custom_myhelp - changes the current directory of the process
  * @info: Structure containing potential arguments. Used to maintain
  *          constant function prototype.
  *  Return: Always 0
  */
-int _myhelp(info_t *info)
+int custom_myhelp(info_t *info)
 {
 	char **arg_array;
 
