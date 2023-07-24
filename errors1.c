2f68d2deea5ef35b9c978cb3d7f5994558456fb4
Mina Robir <menarobir246rm@gmail.com>
2023-07-25 01:59:35 +0300

    Update errors1.c

--- errors1.c
+++ errors1.c
@@ -13,7 +13,7 @@ int _erratoi(char *s)
 
 	if (*s == '+')
 		s++;  /* TODO: why does this make main return 255? */
-	for (i = 0;  s[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] >= '0' && s[i] <= '9')
 		{
@@ -32,35 +32,33 @@ int _erratoi(char *s)
  * print_error - prints an error message
  * @info: the parameter & return info struct
  * @estr: string containing specified error type
- * Return: 0 if no numbers in string, converted number otherwise
- *        -1 on error
+ * Return: void
  */
 void print_error(info_t *info, char *estr)
 {
-	puts(info->fname);
-	puts(": ");
-	printf(info->line_count, STDERR_FILENO);
-	puts(": ");
-	puts(info->argv[0]);
-	puts(": ");
-	puts(estr);
+	_eputs(info->fname);
+	_eputs(": ");
+	print_d(info->line_count, STDERR_FILENO);
+	_eputs(": ");
+	_eputs(info->argv[0]);
+	_eputs(": ");
+	_eputs(estr);
 }
 
 /**
  * print_d - function prints a decimal (integer) number (base 10)
  * @input: the input
  * @fd: the filedescriptor to write to
- *
  * Return: number of characters printed
  */
 int print_d(int input, int fd)
 {
-	int (*__putchar)(char) = __putchar;
+	int (*__putchar)(char) = _putchar;
 	int i, count = 0;
 	unsigned int _abs_, current;
 
 	if (fd == STDERR_FILENO)
-		__putchar = __putchar;
+		__putchar = _eputchar;
 	if (input < 0)
 	{
 		_abs_ = -input;
@@ -90,7 +88,6 @@ int print_d(int input, int fd)
  * @num: number
  * @base: base
  * @flags: argument flags
- *
  * Return: string
  */
 char *convert_number(long int num, int base, int flags)
@@ -105,13 +102,13 @@ char *convert_number(long int num, int base, int flags)
 	{
 		n = -num;
 		sign = '-';
-
 	}
 	array = flags & CONVERT_LOWERCASE ? "0123456789abcdef" : "0123456789ABCDEF";
 	ptr = &buffer[49];
 	*ptr = '\0';
 
-	do	{
+	do
+	{
 		*--ptr = array[n % base];
 		n /= base;
 	} while (n != 0);
@@ -124,17 +121,108 @@ char *convert_number(long int num, int base, int flags)
 /**
  * remove_comments - function replaces first instance of '#' with '\0'
  * @buf: address of the string to modify
- *
- * Return: Always 0;
+ * Return: void
  */
 void remove_comments(char *buf)
 {
 	int i;
 
 	for (i = 0; buf[i] != '\0'; i++)
+	{
 		if (buf[i] == '#' && (!i || buf[i - 1] == ' '))
 		{
 			buf[i] = '\0';
 			break;
 		}
+	}
+}
+
+/**
+ * my_print_env - prints the current environment variables
+ * @info: Structure containing potential arguments. Used to maintain
+ *         constant function prototype.
+ * Return: Always 0
+ */
+int my_print_env(info_t *info)
+{
+	print_list_string(info->env);
+	return (0);
+}
+
+/**
+ * get_env_value - gets the value of an environment variable
+ * @info: Structure containing potential arguments. Used to maintain
+ * @name: environment variable name
+ *
+ * Return: the value of the environment variable or NULL if not found
+ */
+char *get_env_value(info_t *info, const char *name)
+{
+	list_t *node = info->env;
+	char *value;
+
+	while (node)
+	{
+		value = starts_with(node->str, name);
+		if (value && *value)
+			return (value);
+		node = node->next;
+	}
+	return (NULL);
+}
+
+/**
+ * my_set_env - Initialize a new environment variable or modify an existing one
+ * @info: Structure containing potential arguments. Used to maintain
+ *         constant function prototype.
+ * Return: Always 0
+ */
+int my_set_env(info_t *info)
+{
+	if (info->argc != 3)
+	{
+		puts("Incorrect number of arguments\n");
+		return (1);
+	}
+	if (set_environment_variable(info, info->argv[1], info->argv[2]))
+		return (0);
+	return (1);
+}
+
+/**
+ * my_unset_env - Remove an environment variable
+ * @info: Structure containing potential arguments. Used to maintain
+ *            constant function prototype.
+ * Return: Always 0
+ */
+int my_unset_env(info_t *info)
+{
+	int i;
+
+	if (info->argc == 1)
+	{
+		puts("Too few arguments.\n");
+		return (1);
+	}
+	for (i = 1; i < info->argc; i++)
+		unset_environment_variable(info, info->argv[i]);
+
+	return (0);
+}
+
+/**
+ * initialize_env_list - populates env linked list with the current environment
+ * @info: Structure containing potential arguments. Used to maintain
+ *           constant function prototype.
+ * Return: Always 0
+ */
+int initialize_env_list(info_t *info)
+{
+	list_t *node = NULL;
+	size_t i;
+
+	for (i = 0; environ[i]; i++)
+		add_node_end(&node, environ[i], 0);
+	info->env = node;
+	return (0);
 }
