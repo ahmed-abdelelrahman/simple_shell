d3113838c2dfdfa0dfa489b088927e0d038a8546
Mina Robir <menarobir246rm@gmail.com>
2023-07-25 02:08:06 +0300

    Update errors1.c

--- errors1.c
+++ errors1.c
@@ -3,7 +3,7 @@
 /**
  * _erratoi - converts a string to an integer
  * @s: the string to be converted
- * Return: 0 if no numbers in string, the converted number otherwise
+ * Return: 0 if no numbers in the string, the converted number otherwise
  *       -1 on error
  */
 int _erratoi(char *s)
@@ -49,7 +49,7 @@ void print_error(info_t *info, char *estr)
  * print_d - function prints a decimal (integer) number (base 10)
  * @input: the input
  * @fd: the file descriptor to write to
- * Return: the number of characters printed
+ * Return: number of characters printed
  */
 int print_d(int input, int fd)
 {
@@ -121,7 +121,7 @@ char *convert_number(long int num, int base, int flags)
 /**
  * remove_comments - function replaces the first instance of '#' with '\0'
  * @buf: address of the string to modify
- * Return: void
+ * Return: Always 0
  */
 void remove_comments(char *buf)
 {
@@ -135,94 +135,4 @@ void remove_comments(char *buf)
 			break;
 		}
 	}
-}
-
-/**
- * my_print_env - prints the current environment variables
- * @info: Structure containing potential arguments. Used to maintain
- *         constant function prototype.
- * Return: Always 0
- */
-int my_print_env(info_t *info)
-{
-	print_list_string(info->env);
-	return (0);
-}
-
-/**
- * get_env_value - gets the value of an environment variable
- * @info: Structure containing potential arguments. Used to maintain
- * @name: environment variable name
- *
- * Return: the value of the environment variable or NULL if not found
- */
-char *get_env_value(info_t *info, const char *name)
-{
-	list_t *node = info->env;
-	char *value;
-
-	while (node)
-	{
-		value = starts_with(node->str, name);
-		if (value && *value)
-			return (value);
-		node = node->next;
-	}
-	return (NULL);
-}
-
-/**
- * my_set_env - Initialize a new environment variable or modify an existing one
- * @info: Structure containing potential arguments. Used to maintain
- *         constant function prototype.
- * Return: Always 0
- */
-int my_set_env(info_t *info)
-{
-	if (info->argc != 3)
-	{
-		puts("Incorrect number of arguments\n");
-		return (1);
-	}
-	if (set_environment_variable(info, info->argv[1], info->argv[2]))
-		return (0);
-	return (1);
-}
-
-/**
- * my_unset_env - Remove an environment variable
- * @info: Structure containing potential arguments. Used to maintain
- *            constant function prototype.
- * Return: Always 0
- */
-int my_unset_env(info_t *info)
-{
-	int i;
-
-	if (info->argc == 1)
-	{
-		puts("Too few arguments.\n");
-		return (1);
-	}
-	for (i = 1; i < info->argc; i++)
-		unset_environment_variable(info, info->argv[i]);
-
-	return (0);
-}
-
-/**
- * initialize_env_list - populates the env linked list with the current environment
- * @info: Structure containing potential arguments. Used to maintain
- *           constant function prototype.
- * Return: Always 0
- */
-int initialize_env_list(info_t *info)
-{
-	list_t *node = NULL;
-	size_t i;
-
-	for (i = 0; environ[i]; i++)
-		add_node_end(&node, environ[i], 0);
-	info->env = node;
-	return (0);
 }
