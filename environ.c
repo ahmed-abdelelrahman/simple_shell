a23fa6604323fba93ccd62366b5a051d581e1e9d
Mina Robir <menarobir246rm@gmail.com>
2023-07-27 05:08:35 +0300

    Update environ.c

--- environ.c
+++ environ.c
@@ -1,36 +1,6 @@
 #include "shell.h"
 
 /**
- * print_string - Traverse and print the strings in the linked list.
- * @head: Pointer to the head node of the linked list.
- */
-void print_string(const list_t *head) {
-    const list_t *current = head;
-    while (current) {
-        printf("%s\n", current->str);
-        current = current->next;
-    }
-}
-
-/**
- * starts_with - Check if a string starts with a given prefix.
- * @haystack: The input string to search.
- * @needle: The prefix to check for.
- *
- * Return: A pointer to the location where the prefix starts, or NULL if not found.
- */
-const char *starts_with(const char *haystack, const char *needle) {
-    size_t len_needle = strlen(needle);
-    while (*haystack) {
-        if (strncmp(haystack, needle, len_needle) == 0) {
-            return haystack;
-        }
-        haystack++;
-    }
-    return NULL;
-}
-
-/**
  * _myenv - prints the current environment
  * @info: Structure containing potential arguments. Used to maintain
  *          constant function prototype.
@@ -116,7 +86,7 @@ int populate_env_list(info_t *info)
 	size_t i;
 
 	for (i = 0; environ[i]; i++)
-		add_node_to_end(&node, environ[i], 0);
+		add_node_end(&node, environ[i], 0);
 	info->env = node;
 	return (0);
 }
