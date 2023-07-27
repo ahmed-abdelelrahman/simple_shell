83b65b3202b5e611597c167139f9c183e882ed13
Mina Robir <menarobir246rm@gmail.com>
2023-07-27 03:59:37 +0300

    Update shell.h

--- shell.h
+++ shell.h
@@ -224,9 +224,9 @@ int build_history_list(info_t *info, char *buf, int linecount);
 int renumber_history(info_t *info);
 
 /* my_lists.c */
-list_t *add_node(list_t **, const char *, int);
-list_t *add_node_end(list_t **, const char *, int);
-size_t print_list_str(const list_t *);
+list_t *add_node_to_start(list_t **, const char *, int);
+list_t *add_node_to_end(list_t **, const char *, int);
+size_t print_list_data(const list_t *);
 int delete_node_at_index(list_t **, unsigned int);
 void free_list(list_t **);
 
