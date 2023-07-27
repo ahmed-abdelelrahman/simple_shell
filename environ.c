a3b0d47e2819873cd970f19029883aa75b990fa6
Mina Robir <menarobir246rm@gmail.com>
2023-07-27 05:15:15 +0300

    Update environ.c

--- environ.c
+++ environ.c
@@ -1,16 +1,30 @@
 #include "shell.h"
 
 /**
- * _myenv - prints the current environment
+ * _myunsetenv - Remove an environment variable
  * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
+ *        constant function prototype.
  * Return: Always 0
  */
-int _myenv(info_t *info)
+int _myunsetenv(info_t *info)
 {
-	print_list_data(info->env);
+	int i;
+
+	if (info->argc == 1)
+	{
+		_eputs("Too few arguments.\n");
+		return (1);
+	}
+	for (i = 1; i < info->argc; i++) // Use '<' instead of '<='
+	{
+		char *env_var = _getenv(info, info->argv[i]);
+		if (env_var)
+			unsetenv(env_var);
+	}
+
 	return (0);
 }
+
 
 /**
  * _getenv - gets the value of an environ variable
