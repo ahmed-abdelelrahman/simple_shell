3ca5abaf963f9991763c6ef6e5acf97066d713af
Mina Robir <menarobir246rm@gmail.com>
2023-07-27 03:42:43 +0300

    Update lists.c

--- lists.c
+++ lists.c
@@ -1,37 +1,6 @@
 #include "shell.h"
 
 /**
- * create_node - creates a new node with given data
- * @data: data to be stored in the node
- *
- * Return: pointer to the new node
- */
-list_t *create_node(const char *data, int index)
-{
-	list_t *new_node;
-
-	new_node = malloc(sizeof(list_t));
-	if (!new_node)
-		return (NULL);
-
-	_memset((void *)new_node, 0, sizeof(list_t));
-	new_node->num = index;
-
-	if (data)
-	{
-		new_node->str = _strdup(data);
-		if (!new_node->str)
-		{
-			free(new_node);
-			return (NULL);
-		}
-	}
-
-	new_node->next = NULL;
-	return (new_node);
-}
-
-/**
  * add_node_to_start - adds a node to the start of the list
  * @head: address of pointer to head node
  * @data: data to be stored in the new node
