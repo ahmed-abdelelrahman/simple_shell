b739ffa395092a063e05ef72976fd20ed878c491
Mina Robir <menarobir246rm@gmail.com>
2023-07-28 02:48:49 +0300

    Update builtin1.c

--- builtin1.c
+++ builtin1.c
@@ -22,6 +22,59 @@ char *_strchr(char *s, char c)
 }
 
 /**
+ * print_alias - prints an alias string
+ * @node: the alias node
+ */
+void print_alias(list_t *node)
+{
+	char *p = NULL, *a = NULL;
+
+	if (node)
+	{
+		p = _strchr(node->str, '=');
+		for (a = node->str; a <= p; a++)
+			_putchar(*a);
+		_putchar('\'');
+		_puts(p + 1);
+		_puts("'\n");
+	}
+}
+
+/**
+ * _myalias - mimics the alias builtin (man alias)
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ * Return: Always 0
+ */
+int _myalias(info_t *info)
+{
+	int i = 0;
+	char *p = NULL;
+	list_t *node = NULL;
+
+	if (info->argc == 1)
+	{
+		node = info->alias;
+		while (node)
+		{
+			print_alias(node);
+			node = node->next;
+		}
+		return (0);
+	}
+	for (i = 1; info->argv[i]; i++)
+	{
+		p = _strchr(info->argv[i], '=');
+		if (p)
+			set_alias(info, info->argv[i]);
+		else
+			print_alias(node_starts_with(info->alias, info->argv[i], '='));
+	}
+
+	return (0);
+}
+
+/**
  * _putchar - Writes a character to the standard output (stdout)
  * @c: The character to be written
  *
