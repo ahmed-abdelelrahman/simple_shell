17187a857f7f29c318fef1595676fc2caff587d3
minarob23 <menarobir246rm@gmail.com>
2023-07-26 18:32:41 -0700

    m

--- builtin.c
+++ builtin.c
@@ -1,13 +1,13 @@
 #include "shell.h"
 
 /**
- * custom_myexit - exits the shell
+ * _myexit - exits the shell
  * @info: Structure containing potential arguments. Used to maintain
  *          constant function prototype.
  *  Return: exits with a given exit status
  *         (0) if info.argv[0] != "exit"
  */
-int custom_myexit(info_t *info)
+int _myexit(info_t *info)
 {
 	int exitcheck;
 
@@ -18,8 +18,8 @@ int custom_myexit(info_t *info)
 		{
 			info->status = 2;
 			print_error(info, "Illegal number: ");
-			puts(info->argv[1]);
-			putchar('\n');
+			_eputs(info->argv[1]);
+			_eputchar('\n');
 			return (1);
 		}
 		info->err_num = _erratoi(info->argv[1]);
@@ -30,22 +30,22 @@ int custom_myexit(info_t *info)
 }
 
 /**
- * custom_mycd - changes the current directory of the process
+ * _mycd - changes the current directory of the process
  * @info: Structure containing potential arguments. Used to maintain
  *          constant function prototype.
  *  Return: Always 0
  */
-int custom_mycd(info_t *info)
+int _mycd(info_t *info)
 {
 	char *s, *dir, buffer[1024];
 	int chdir_ret;
 
 	s = getcwd(buffer, 1024);
 	if (!s)
-		puts("TODO: >>getcwd failure emsg here<<\n");
+		_puts("TODO: >>getcwd failure emsg here<<\n");
 	if (!info->argv[1])
 	{
-		dir = getenv(info, "HOME=");
+		dir = _getenv(info, "HOME=");
 		if (!dir)
 			chdir_ret = /* TODO: what should this be? */
 				chdir((dir = _getenv(info, "PWD=")) ? dir : "/");
@@ -57,7 +57,7 @@ int custom_mycd(info_t *info)
 		if (!_getenv(info, "OLDPWD="))
 		{
 			_puts(s);
-			putchar('\n');
+			_putchar('\n');
 			return (1);
 		}
 		_puts(_getenv(info, "OLDPWD=")), _putchar('\n');
@@ -73,19 +73,19 @@ int custom_mycd(info_t *info)
 	}
 	else
 	{
-		setenv(info, "OLDPWD", _getenv(info, "PWD="));
+		_setenv(info, "OLDPWD", _getenv(info, "PWD="));
 		_setenv(info, "PWD", getcwd(buffer, 1024));
 	}
 	return (0);
 }
 
 /**
- * custom_myhelp - changes the current directory of the process
+ * _myhelp - changes the current directory of the process
  * @info: Structure containing potential arguments. Used to maintain
  *          constant function prototype.
  *  Return: Always 0
  */
-int custom_myhelp(info_t *info)
+int _myhelp(info_t *info)
 {
 	char **arg_array;
 
