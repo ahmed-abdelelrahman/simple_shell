69e70fdc709e09afd309ed3bcbabcb8e951046c6
Mina Robir <menarobir246rm@gmail.com>
2023-07-27 05:19:59 +0300

    Update environ.c

--- environ.c
+++ environ.c
@@ -1,30 +1,16 @@
 #include "shell.h"
 
 /**
- * _myunsetenv - Remove an environment variable
+ * _myenv - prints the current environment
  * @info: Structure containing potential arguments. Used to maintain
- *        constant function prototype.
+ *          constant function prototype.
  * Return: Always 0
  */
-int _myunsetenv(info_t *info)
+int _myenv(info_t *info)
 {
-	int i;
-
-	if (info->argc == 1)
-	{
-		_eputs("Too few arguments.\n");
-		return (1);
-	}
-	for (i = 1; i < info->argc; i++) // Use '<' instead of '<='
-	{
-		char *env_var = _getenv(info, info->argv[i]);
-		if (env_var)
-			unsetenv(env_var);
-	}
-
+	print_list_str(info->env);
 	return (0);
 }
-
 
 /**
  * _getenv - gets the value of an environ variable
@@ -83,7 +69,7 @@ int _myunsetenv(info_t *info)
 		return (1);
 	}
 	for (i = 1; i <= info->argc; i++)
-		unsetenv(info, info->argv[i]);
+		_unsetenv(info, info->argv[i]);
 
 	return (0);
 }
@@ -100,7 +86,7 @@ int populate_env_list(info_t *info)
 	size_t i;
 
 	for (i = 0; environ[i]; i++)
-		add_node_to_end(&node, environ[i], 0);
+		add_node_end(&node, environ[i], 0);
 	info->env = node;
 	return (0);
 }
