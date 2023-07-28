6c3d2cd94614d5265682e7427f2c8c824b38c927
Mina Robir <menarobir246rm@gmail.com>
2023-07-29 02:18:11 +0300

    Update shell.h

--- shell.h
+++ shell.h
@@ -118,9 +118,9 @@ void find_cmd(info_t *);
 void fork_cmd(info_t *);
 
 /* my_parser.c */
-int is_cmd(info_t *, char *);
-char *dup_chars(char *, int, int);
-char *find_path(info_t *, char *, char *);
+int execue_file(info_t *, char *);
+char *duplo_chars(char *, int, int);
+char *see_full_path(info_t *, char *, char *);
 
 /* loophsh.c */
 int loophsh(char **);
