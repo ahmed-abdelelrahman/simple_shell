018725b34f7a5789dc579bd826ab4bd190439039
Mina Robir <menarobir246rm@gmail.com>
2023-07-29 00:55:21 +0300

    Update and rename getinfo.c to info.c

--- info.c
+++ info.c
@@ -1,10 +1,10 @@
 #include "shell.h"
 
 /**
- * clear_info - initializes info_t struct
+ * remove_info - initializes info_t struct
  * @info: struct address
  */
-void clear_info(info_t *info)
+void remove_info(info_t *info)
 {
 	info->arg = NULL;
 	info->argv = NULL;
@@ -13,11 +13,11 @@ void clear_info(info_t *info)
 }
 
 /**
- * set_info - initializes info_t struct
+ * init_info - initializes info_t struct
  * @info: struct address
  * @av: argument vector
  */
-void set_info(info_t *info, char **av)
+void init_info(info_t *info, char **av)
 {
 	int i = 0;
 
@@ -45,11 +45,11 @@ void set_info(info_t *info, char **av)
 }
 
 /**
- * free_info - frees info_t struct fields
+ * free_info_t - frees info_t struct fields
  * @info: struct address
  * @all: true if freeing all fields
  */
-void free_info(info_t *info, int all)
+void free_info_t(info_t *info, int all)
 {
 	ffree(info->argv);
 	info->argv = NULL;
