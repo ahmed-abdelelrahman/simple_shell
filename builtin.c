3949412c92ca8d95ab05acd62310a6db25bc6dde
Mina Robir <menarobir246rm@gmail.com>
2023-07-29 01:12:04 +0300

    Update builtin.c

--- builtin.c
+++ builtin.c
@@ -1,97 +1,97 @@
 #include "shell.h"
 
 /**
- * _myexit - exits the shell
+ * mina_robir_myexit - exits the shell
  * @info: Structure containing potential arguments. Used to maintain
  *          constant function prototype.
  *  Return: exits with a given exit status
  *         (0) if info.argv[0] != "exit"
  */
-int _myexit(info_t *info)
+int mina_robir_myexit(inforobir_t *inforobir)
 {
-	int exitcheck;
+	int robir_exitcheck;
 
-	if (info->argv[1])  /* If there is an exit arguement */
+	if (inforobir->argv[1])  /* If there is an exit arguement */
 	{
-		exitcheck = _erratoi(info->argv[1]);
-		if (exitcheck == -1)
+		robir_exitcheck = _erratoi(inforobir->argv[1]);
+		if (robir_exitcheck == -1)
 		{
-			info->status = 2;
-			print_error(info, "Illegal number: ");
-			_eputs(info->argv[1]);
+			inforobir->status = 2;
+			print_error(inforobir, "Illegal number: ");
+			_eputs(inforobir->argv[1]);
 			_eputchar('\n');
 			return (1);
 		}
-		info->err_num = _erratoi(info->argv[1]);
+		inforobir->err_num = _erratoi(inforobir->argv[1]);
 		return (-2);
 	}
-	info->err_num = -1;
+	inforobir->err_num = -1;
 	return (-2);
 }
 
 /**
- * _mycd - changes the current directory of the process
+ * robir_mycd - changes the current directory of the process
  * @info: Structure containing potential arguments. Used to maintain
  *          constant function prototype.
  *  Return: Always 0
  */
-int _mycd(info_t *info)
+int robir_mycd(inforobir_t *inforobir)
 {
-	char *s, *dir, buffer[1024];
-	int chdir_ret;
+	char *q, *directory, robir_buffer[1024];
+	int chdirmena_ret;
 
-	s = getcwd(buffer, 1024);
-	if (!s)
+	q = getcwd(robir_buffer, 1024);
+	if (!q)
 		_puts("TODO: >>getcwd failure emsg here<<\n");
-	if (!info->argv[1])
+	if (!inforobir->argv[1])
 	{
-		dir = _getenv(info, "HOME=");
-		if (!dir)
-			chdir_ret = /* TODO: what should this be? */
-				chdir((dir = _getenv(info, "PWD=")) ? dir : "/");
+		directory = _getenv(info, "HOME=");
+		if (!directory)
+			chdirmena_ret = /* TODO: what should this be? */
+				chdir((directory = _getenv(inforobir, "PWD=")) ? directory : "/");
 		else
-			chdir_ret = chdir(dir);
+			chdirmena_ret = chdir(directory);
 	}
-	else if (_strcmp(info->argv[1], "-") == 0)
+	else if (_strcmp(inforobir->argv[1], "-") == 0)
 	{
-		if (!_getenv(info, "OLDPWD="))
+		if (!_getenv(inforobir, "OLDPWD="))
 		{
 			_puts(s);
 			_putchar('\n');
 			return (1);
 		}
-		_puts(_getenv(info, "OLDPWD=")), _putchar('\n');
-		chdir_ret = /* TODO: what should this be? */
-			chdir((dir = _getenv(info, "OLDPWD=")) ? dir : "/");
+		_puts(_getenv(inforobir, "OLDPWD=")), _putchar('\n');
+		chdirmena_ret = /* TODO: what should this be? */
+			chdir((directory = _getenv(inforobir, "OLDPWD=")) ? directory : "/");
 	}
 	else
-		chdir_ret = chdir(info->argv[1]);
-	if (chdir_ret == -1)
+		chdirmena_ret = chdir(inforobir->argv[1]);
+	if (chdirmena_ret == -1)
 	{
-		print_error(info, "can't cd to ");
-		_eputs(info->argv[1]), _eputchar('\n');
+		print_error(inforobir, "can't cd to ");
+		_eputs(inforobir->argv[1]), _eputchar('\n');
 	}
 	else
 	{
-		_setenv(info, "OLDPWD", _getenv(info, "PWD="));
-		_setenv(info, "PWD", getcwd(buffer, 1024));
+		_setenv(inforobir, "OLDPWD", _getenv(inforobir, "PWD="));
+		_setenv(inforobir, "PWD", getcwd(robir_buffer, 1024));
 	}
 	return (0);
 }
 
 /**
- * _myhelp - changes the current directory of the process
+ * robir_myhelp - changes the current directory of the process
  * @info: Structure containing potential arguments. Used to maintain
  *          constant function prototype.
  *  Return: Always 0
  */
-int _myhelp(info_t *info)
+int robir_myhelp(info_t *info)
 {
-	char **arg_array;
+	char **robir_arg_array;
 
-	arg_array = info->argv;
+	robir_arg_array = info->argv;
 	_puts("help call works. Function not yet implemented \n");
 	if (0)
-		_puts(*arg_array); /* temp att_unused workaround */
+		_puts(*robir_arg_array); /* temp att_unused workaround */
 	return (0);
 }
