0d6fc10756faef1253853de00ad6696f1a362d32
Mina Robir <menarobir246rm@gmail.com>
2023-07-29 02:13:52 +0300

    Update exits.c

--- exits.c
+++ exits.c
@@ -1,13 +1,13 @@
 #include "shell.h"
 
 /**
- **_strncpy - copies a string
+ ** copy_string - copies a string
  *@dest: the destination string to be copied to
  *@src: the source string
  *@n: the amount of characters to be copied
  *Return: the concatenated string
  */
-char *_strncpy(char *dest, char *src, int n)
+char *copy_string(char *dest, char *src, int n)
 {
 	int i, j;
 	char *s = dest;
@@ -31,13 +31,13 @@ char *_strncpy(char *dest, char *src, int n)
 }
 
 /**
- **_strncat - concatenates two strings
+ **conc_string - concatenates two strings
  *@dest: the first string
  *@src: the second string
  *@n: the amount of bytes to be maximally used
  *Return: the concatenated string
  */
-char *_strncat(char *dest, char *src, int n)
+char *conc_string(char *dest, char *src, int n)
 {
 	int i, j;
 	char *s = dest;
@@ -58,12 +58,12 @@ char *_strncat(char *dest, char *src, int n)
 }
 
 /**
- **_strchr - locates a character in a string
+ **locate_string - locates a character in a string
  *@s: the string to be parsed
  *@c: the character to look for
  *Return: (s) a pointer to the memory area s
  */
-char *_strchr(char *s, char c)
+char *locate_string(char *s, char c)
 {
 	do {
 		if (*s == c)
