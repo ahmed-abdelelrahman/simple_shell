6c3525a0c357cdac2106e7a1ef04a0ef20f86a7e
Mina Robir <menarobir246rm@gmail.com>
2023-07-29 01:33:10 +0300

    Update and rename lists.c to list.c

--- list.c
+++ list.c
@@ -1,14 +1,14 @@
 #include "shell.h"
 
 /**
- * add_node - adds a node to the start of the list
+ * add_node_from_start - adds a node to the start of the list
  * @head: address of pointer to head node
  * @str: str field of node
  * @num: node index used by history
  *
  * Return: size of list
  */
-list_t *add_node(list_t **head, const char *str, int num)
+list_t *add_node_from_start(list_t **head, const char *str, int num)
 {
 	list_t *new_head;
 
@@ -34,14 +34,14 @@ list_t *add_node(list_t **head, const char *str, int num)
 }
 
 /**
- * add_node_end - adds a node to the end of the list
+ * add_node_to_end - adds a node to the end of the list
  * @head: address of pointer to head node
  * @str: str field of node
  * @num: node index used by history
  *
  * Return: size of list
  */
-list_t *add_node_end(list_t **head, const char *str, int num)
+list_t *add_node_to_end(list_t **head, const char *str, int num)
 {
 	list_t *new_node, *node;
 
@@ -75,12 +75,12 @@ list_t *add_node_end(list_t **head, const char *str, int num)
 }
 
 /**
- * print_list_str - prints only the str element of a list_t linked list
+ * print_list_string - prints only the string element of a list_t linked list
  * @h: pointer to first node
  *
  * Return: size of list
  */
-size_t print_list_str(const list_t *h)
+size_t print_list_string(const list_t *h)
 {
 	size_t i = 0;
 
@@ -135,12 +135,12 @@ int delete_node_at_index(list_t **head, unsigned int index)
 }
 
 /**
- * free_list - frees all nodes of a list
+ * free_list_nodes_mina - frees all nodes of a list
  * @head_ptr: address of pointer to head node
  *
  * Return: void
  */
-void free_list(list_t **head_ptr)
+void free_list_nodes_mina(list_t **head_ptr)
 {
 	list_t *node, *next_node, *head;
 
