80afa103e99dade89f03abb09b8a09e173e97d1f
Mina Robir <menarobir246rm@gmail.com>
2023-07-28 03:56:35 +0300

    Update getviron.c

--- getviron.c
+++ getviron.c
@@ -8,13 +8,13 @@
  */
 char **get_environment_variable(info_t *info)
 {
-	if (!info->environ || info->env_changed)
-	{
-		info->environ = list_to_strings(info->env);
-		info->env_changed = 0;
-	}
+    if (!info->environ || info->env_changed)
+    {
+        info->environ = list_to_strings(info->env);
+        info->env_changed = 0;
+    }
 
-	return (info->environ);
+    return (info->environ);
 }
 
 /**
@@ -26,27 +26,27 @@ char **get_environment_variable(info_t *info)
  */
 int unset_environment_variable(info_t *info, char *var)
 {
-	list_t *node = info->env;
-	size_t i = 0;
-	const char *p;
+    list_t *node = info->env;
+    size_t i = 0;
+    char *p;
 
-	if (!node || !var)
-		return (0);
+    if (!node || !var)
+        return (0);
 
-	while (node)
-	{
-		p = starts_with(node->str, var);
-		if (p && *p == '=')
-		{
-			info->env_changed = delete_node_at_index(&(info->env), i);
-			i = 0;
-			node = info->env;
-			continue;
-		}
-		node = node->next;
-		i++;
-	}
-	return (info->env_changed);
+    while (node)
+    {
+        p = starts_with(node->str, var);
+        if (p && *p == '=')
+        {
+            info->env_changed = delete_node_at_index(&(info->env), i);
+            i = 0;
+            node = info->env;
+            continue;
+        }
+        node = node->next;
+        i++;
+    }
+    return (info->env_changed);
 }
 
 /**
@@ -60,35 +60,35 @@ int unset_environment_variable(info_t *info, char *var)
  */
 int add_environment_variable(info_t *info, char *var, char *value)
 {
-	char *buf = NULL;
-	list_t *node;
-	char *p;
+    char *buf = NULL;
+    list_t *node;
+    char *p;
 
-	if (!var || !value)
-		return (0);
+    if (!var || !value)
+        return (0);
 
-	buf = malloc(_strlen(var) + _strlen(value) + 2);
-	if (!buf)
-		return (1);
-	strcpy(buf, var); // Change _strcpy to strcpy
-	strcat(buf, "=");
-	strcat(buf, value);
-	node = info->env;
-	while (node)
-	{
-		p = starts_with(node->str, var);
-		if (p && *p == '=')
-		{
-			free(node->str);
-			node->str = buf;
-			info->env_changed = 1;
-			return (0);
-		}
-		node = node->next;
-	}
-	add_node_end(&(info->env), buf, 0);
-	free(buf);
-	info->env_changed = 1;
-	return (0);
+    buf = malloc(_strlen(var) + _strlen(value) + 2);
+    if (!buf)
+        return (1);
+    strcpy(buf, var);
+    strcat(buf, "=");
+    strcat(buf, value);
+    node = info->env;
+    while (node)
+    {
+        p = starts_with(node->str, var);
+        if (p && *p == '=')
+        {
+            free(node->str);
+            node->str = buf;
+            info->env_changed = 1;
+            return (0);
+        }
+        node = node->next;
+    }
+    add_node_end(&(info->env), buf, 0);
+    free(buf);
+    info->env_changed = 1;
+    return (0);
 }
 
