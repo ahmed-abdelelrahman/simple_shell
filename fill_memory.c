967658d1cf9cd2ec47a12a56683bae517f64a936
Mina Robir <menarobir246rm@gmail.com>
2023-07-29 02:35:01 +0300

    Update and rename fillmemory.c to fill_memory.c

--- fill_memory.c
+++ fill_memory.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 /**
- **_memset - fills memory with a constant byte
+ **_memset_memory - fills memory with a constant byte
  *@s: the pointer to the memory area
  *@b: the byte to fill *s with
  *@n: the amount of bytes to be filled
@@ -17,7 +17,7 @@ char *_memset_memory(char *s, char b, unsigned int n)
 }
 
 /**
- * ffree - frees a string of strings
+ * ffree_string - frees a string of strings
  * @pp: string of strings
  */
 void ffree_string(char **pp)
@@ -32,7 +32,7 @@ void ffree_string(char **pp)
 }
 
 /**
- * _realloc - reallocates a block of memory
+ * _reallocs_block - reallocates a block of memory
  * @ptr: pointer to previous malloc'ated block
  * @old_size: byte size of previous block
  * @new_size: byte size of new block
